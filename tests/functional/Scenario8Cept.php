aa2a475fe24f43257790e19bc5c8382fbdf52ecc
Pol Dellaiera <pol.dellaiera@protonmail.com>
2019-07-28 23:02:44 +0200

    Add tests.

---
+++ tests/functional/Scenario8Cept.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types = 1);
+
+$I = new FunctionalTester($scenario);
+$I->wantTo('A custom dynamic task can be run.');
+$I->amInPath(realpath(__DIR__ . '/fixtures/test8'));
+
+$I->runShellCommand('composer install -n --no-progress --no-scripts --no-dev --no-suggest --no-ansi');
+
+$I->runShellCommand('../../../_output/vendor/bin/taskman boo');
+$I->canSeeInShellOutput('');
+$I->dontSeeInShellOutput('boo boo boo');

056e3fc8f503b86061938af2b6b0af3260570af2
Pol Dellaiera <pol.dellaiera@protonmail.com>
2021-03-26 10:15:34 +0100

    refactor: Add humgub/box minimal configuration files.

--- src/Taskman.php
+++ src/Taskman.php
@@ -22,7 +22,7 @@ final class Taskman
 {
     public const APPLICATION_NAME = 'Taskman';
 
-    public const VERSION = '@git_commit_short@';
+    public const VERSION = 'dev-master';
 
     /**
      * Create default configuration.

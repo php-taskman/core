568540b12e22f273d3aa4d121182d7be2dc91e9e
Pol Dellaiera <pol.dellaiera@protonmail.com>
2021-03-26 09:50:19 +0100

    refactor: Add humgub/box minimal configuration files.

---
+++ box/scoper.inc.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'whitelist' => [
+        'PhpTaskman\*',
+        'PhpParser\*',
+        'Composer\*',
+    ],
+    'patchers' => [
+        // Remove scoper namespace prefix from Symfony polyfills namespace
+        static function (string $filePath, string $prefix, string $contents): string {
+            if (!preg_match('{vendor/symfony/polyfill[^/]*/bootstrap.php}i', $filePath)) {
+                return $contents;
+            }
+
+            return preg_replace('/namespace .+;/', '', $contents);
+        },
+    ],
+    'whitelist-global-functions' => false,
+  	'whitelist-global-classes' => false,
+];
